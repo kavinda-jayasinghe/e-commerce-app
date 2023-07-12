7133cc68eb2d5fb88982a6c0c1935f76f8e70ac9
kavinda-jayasinghe <kavindajayasinghequn@gmail.com>
2023-07-12 20:43:47 +0530

    upload images api

--- back_end/src/main/java/com/example/back_end/entity/Product.java
+++ back_end/src/main/java/com/example/back_end/entity/Product.java
@@ -2,20 +2,28 @@ package com.example.back_end.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer productId;
-
     private String productName;
     private String productDescription;
     private Double productDiscountedPrice;
     private Double productActualPrice;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "product_images",
+            joinColumns = {
+                    @JoinColumn(name = "product_id")
+            },
+            inverseJoinColumns = {
+            @JoinColumn(name="image_id")
+            }
+    )
+    private Set<ImageModel> productImages;
 }
